6f042c03ffbed05dc638250a0c237c96c3a634c4
HoussamBenali <houssambenali@gmail.com>
2021-05-18 22:20:08 +0200

    Nearly Working

--- database/migrations/2021_05_17_165049_games_offline.php
+++ database/migrations/2021_05_17_165049_games_offline.php
@@ -13,15 +13,15 @@ class GamesOffline extends Migration
      */
     public function up()
     {
-        Schema::create('games_offline', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('user_id')->constrained('users');
-            $table->integer('rewards')->nullable();
-            $table->integer('wins')->nullable();
-            $table->integer('coins')->default('0');
-            $table->dateTime('created_at');
-            $table->dateTime('updated_at');
-        });
+        // Schema::create('games_offline', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->foreignId('user_id')->constrained('users');
+        //     $table->integer('rewards')->nullable();
+        //     $table->integer('wins')->nullable();
+        //     $table->integer('coins')->default('0');
+        //     $table->dateTime('created_at');
+        //     $table->dateTime('updated_at');
+        // });
     }
 
     /**
